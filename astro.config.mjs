dab7e2d42661e7ce0d16974d3ada740ec9858dd3
Suprnova <nova@suprnova.dev>
2024-03-17 16:56:54 -0400

    Finish migrating to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        ic: ["baseline-access-time", "outline-calendar-month"]
+      }
+    }),
+    mdx()],
   site: 'https://suprnova.dev'
 });
